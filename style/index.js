cc7e53a04698d081e9669883bd99d5166c92b4db
Haya-2 <ASirot.pro@gmail.com>
2024-12-04 23:07:20 +0100

    Add burger and correct small size problems (buttons, or img)

---
+++ style/index.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const burgerMenu = document.getElementById("burger-menu");
+    const navbar = document.querySelector(".navbar");
+
+    burgerMenu.addEventListener("click", () => {
+        navbar.classList.toggle("active");
+    });
+});
